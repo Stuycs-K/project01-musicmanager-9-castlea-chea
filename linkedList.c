156dc07a9dd686cc171199d207f239aef46ef0e3
root <root@ABs-laptop>
2023-10-23 20:51:47 -0400

    compare function to be tested, added some constants to make the return values of the compare function easier to interpret.

--- linkedList.c
+++ linkedList.c
@@ -1,18 +1,38 @@
 #include "linkedList.h"
+#include <string.h>
+int SONG_LESS = -2; // make constants for future reference
+int SONG_MORE = 2;
+int ARTIST_LESS = -1;
+int ARTIST_MORE = 1;
 /*
 struct song_node insert_front(char name[], char artist[]) {
-    
-}
+
+} */
 
 int song_compare(struct song_node a, struct song_node b) {
+  int artists = strcmp(a.artist,b.artist);
+  if(artists==0){
+    int names = strcmp(a.name,b.name)
+    if(names<0){
+      return SONG_LESS;
+    }
+    else{
+      return SONG_MORE;
+    }
+  }
+  else if(artists<0){
+    return ARTIST_LESS;
+  }
+  else{
+    return ARTIST_MORE;
+  }
+}
+
+struct song_node insert(char name[], char artist[]) {
 
 }
 
-struct song_node insert(char name[], char artist[], index) {
-
-}
-
-void print_list(struct song_node * start) {
+/*void print_list(struct song_node * start) {
 
 }
 
--- linkedList.h
+++ linkedList.h
@@ -1,16 +1,23 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
+int SONG_LESS = -2; // make constants for future reference
+int SONG_MORE = 2;
+int ARTIST_LESS = -1;
+int ARTIST_MORE = 1;
+
 struct song_node {
     char name[128];
     char artist[128];
     struct song_node * next;
-}
+};
+
+struct song_node list[27];
 
 struct song_node insert_front(char name[], char artist[]);
 
 int song_compare(struct song_node a, struct song_node b);
 
-struct song_node insert(char name[], char artist[], index);
+struct song_node insert(char name[], char artist[]);
 
 void print_list(struct song_node * start);
 
