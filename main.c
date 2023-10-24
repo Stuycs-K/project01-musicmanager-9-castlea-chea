05de10881da82007abb2e1103f0058e2b74e0c88
Andrew Che <ache40@stuy.edu>
2023-10-23 23:20:40 -0400

    Added testing for free_list

--- main.c
+++ main.c
@@ -8,8 +8,8 @@ int main() {
     s0 = insert_song(s0, "Hotel California", "Eagles");
     s0 = insert_song(s0, "Rosanna", "TOTO");
     s0 = insert_song(s0, "Livin' on a Prayer", "Bon Jovi");
-    //printf("done");
-    fflush(stdout);
+    print_list(s0);
+    s0 = free_list(s0);
     print_list(s0);
     return 0;
 }
