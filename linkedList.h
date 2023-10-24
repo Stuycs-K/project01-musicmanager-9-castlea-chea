4f575840ac241003a5c87edf2ec475c0e81a8c46
Andrew Che <ache40@stuy.edu>
2023-10-23 23:17:34 -0400

    Changed everything to be node pointers

--- linkedList.h
+++ linkedList.h
@@ -1,9 +1,5 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
-int SONG_LESS = -2; // make constants for future reference
-int SONG_MORE = 2;
-int ARTIST_LESS = -1;
-int ARTIST_MORE = 1;
 
 struct song_node {
     char name[128];
@@ -15,7 +11,7 @@ struct song_node * insert_front(struct song_node * list, char n[], char a[]);
 
 int song_compare(struct song_node a, struct song_node b);
 
-struct song_node insert_song(char name[], char artist[]);
+struct song_node * insert_song(struct song_node * list, char name[], char artist[]);
 
 void print_node(struct song_node * s);
 
