2a3dcfd2b222a0cbe6f47c7d4324d2a6bade66d6
Andrew Che <ache40@stuy.edu>
2023-10-23 23:17:22 -0400

    Added some more testing

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 
 int main() {
     struct song_node * s0 = NULL;
-    s0 = insert_front(s0, "Africa", "TOTO");
-    s0 = insert_front(s0, "Hotel California", "Eagles");
-    s0 = insert_front(s0, "Toccata and Fugue in D minor", "Johann Sebastian Bach");
+    s0 = insert_song(s0, "Africa", "TOTO");
+    s0 = insert_song(s0, "Toccata and Fugue in D minor", "Johann Sebastian Bach");
+    s0 = insert_song(s0, "Hotel California", "Eagles");
+    s0 = insert_song(s0, "Rosanna", "TOTO");
+    s0 = insert_song(s0, "Livin' on a Prayer", "Bon Jovi");
+    //printf("done");
+    fflush(stdout);
     print_list(s0);
     return 0;
 }
