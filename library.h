e733077738b07497f03c1d8cb080b1ea6a0c9089
Andrew Che <ache40@stuy.edu>
2023-10-26 14:40:45 -0400

    Added function headers I've been forgetting

--- library.h
+++ library.h
@@ -3,4 +3,8 @@
 struct song_node** initialize_library();
 void add_song_nodes(struct song_node** library, char name[], char artist[]);
 void print_letter(struct song_node** library, char letter);
+struct song_node* lib_find_song(struct song_node** library, char n[], char a[]);
+struct song_node* lib_find_artist(struct song_node** library, char a[]);
+void lib_remove_song(struct song_node** library, char n[], char a[]);
+void lib_clear(struct song_node** library);
 #endif
