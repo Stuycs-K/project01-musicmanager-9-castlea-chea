91733c1addbc7e3327469b4ec6c74d4e1a4441e2
root <root@ABs-laptop>
2023-10-26 09:55:08 -0400

    adjusted the h file

--- library.h
+++ library.h
@@ -1,5 +1,5 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 struct song_node** initialize_library();
-void add_song_nodes(struct song_node** library, struct song_node* song);
+void add_song_nodes(struct song_node** library, char name[], char artist[]);
 #endif
